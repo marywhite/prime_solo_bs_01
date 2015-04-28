d22c142121cb75075cfc1972e626ab44a7e386b8
maryk.december@gmail.com <maryk.december@gmail.com>
2015-04-28 15:17:40 -0500

    working reposition

--- script.js
+++ script.js
@@ -5,22 +5,60 @@ var apikey = 'e27fa347f5134d05d053b02acb00c1153f892615';
 // It is called after 'search' is executed.
 function searchCallback(results) {
     $(".search-results").empty();
-    var game;
-    var column = 0;
-    var cell = '';
+    // var game;
+    // var column = 0;
+    // var cell = '';
+    //$(".search-results").append('<div class="row"></div>');
+    
     for (var i = 0; i < 9; i++) {
+    	if ((i % 3) == 0) {
+			$(".search-results").append('<div class="row"></div>');
+		}
     	game = results[i];
-    	column++;
     	var button = '<button class="btn btn-sm btn-success remove"> remove content </button>'
-    	var image = '<img class="img-thumbnail hidden-xs hidden-s" src="' + game.image.icon_url + '"/><br>'
-    	cell +=('<div class="col-md-4 well">' + image  + '<p class="lead text-center">' + game.name + '</p><br>' + game.deck + button + '</div>');
-    	if ( column == 3) {
-    		$(".search-results").append('<div class="row">' + cell + '</div>');
-    		column = 0;
-    		cell = '';
-    	}
-    }
+   		var image = '<img class="img-thumbnail hidden-xs hidden-s" src="' + game.image.icon_url + '"/><br>'
+    	var deck = '<div>' + game.deck + '</div>'
+    	var name = '<p class="lead text-center">' + game.name + '</p><br>'
+    	//cell +=('<div class="col-md-4 well">' + image  + name + deck + button + '</div>');
+		$(".row").last().append('<div class="col-md-4 well">' + image  + name + deck + button + '</div>');
+	}
 }
+
+
+
+
+
+
+
+
+//     	column++;
+//     	var button = '<button class="btn btn-sm btn-success remove"> remove content </button>'
+//     	var image = '<img class="img-thumbnail hidden-xs hidden-s" src="' + game.image.icon_url + '"/><br>'
+//     	var deck = '<div class="row">' + game.deck + '</div>'
+//     	var name = '<p class="lead text-center">' + game.name + '</p><br>'
+//     	cell +=('<div class="col-md-4 well">' + image  + name + deck + button + '</div>');
+//     	if ( column == 3) {
+//     		$(".search-results").append('<div class="row">' + cell + '</div>');
+//     		column = 0;
+//     		cell = '';
+//     	}
+//     }
+// }
+
+function reposition (){
+	var cells = $(".search-results").children().children();
+	$(".search-results").empty();
+	$(".search-results").append('<div class="row"></div>');
+
+	for (var i = 0; i < cells.length; i++){
+		if ((i % 3) == 0) {
+			$(".search-results").append('<div class="row"></div>');
+		}
+		$(".row").last().append(cells[i]);
+	}
+}
+	
+
 
 $(document).ready(function() {
 
@@ -29,8 +67,9 @@ $(document).ready(function() {
 		search(searchTerm);
 	})
 
-	$(".search-results").on("click", "img", function() {
+	$(".search-results").on("click", ".remove", function() {
 		$(this).parent().remove();
+		reposition();
 	});
 	
 });
